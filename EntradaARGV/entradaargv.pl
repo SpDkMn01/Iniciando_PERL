190f09a4071625cc5de6ea4fd72d4c981d3c4e2b
Pedro Diaz <pdiazstam@gmail.com>
2020-06-21 11:33:35 -0500

    Entrada de datos $ARGV2

---
+++ EntradaARGV/entradaargv.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $numero1 = $ARGV[0];
+my $numero2 = $ARGV[1];
+
+my $total = $numero1 + $numero2;
+
+print "El total de la suma de $numero1 y $numero2 es igual a $total\n";
