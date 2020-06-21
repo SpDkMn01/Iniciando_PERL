6b7e812e67004bde59d5b799fe27a3afc811f965
Pedro Diaz <pdiazstam@gmail.com>
2020-06-21 12:18:35 -0500

    Array asociativos HASH

---
+++ Hash/hash.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+use strict;
+
+my %imagen = (
+    'ancho' => 200,
+    'altura' => 60,
+    'color_fondo' => 'Azul'
+);
+
+print "Anchura de la imagen: $imagen{'ancho'}\n";
+print "Altura de la imagen: $imagen{'altura'}\n";
+print "Color de fondo: $imagen{'color_fondo'}\n";
