1f029196dfab68b4bc55a30bca028e773b7a6b1b
Pedro Diaz <pdiazstam@gmail.com>
2020-06-21 10:58:22 -0500

    Operadores Aritmeticos

---
+++ OperadoresAritmeticos/operadoresAritmeticos.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+
+use strict;
+
+# Operadores aritmeticos:
+# Suma +
+# Resta -
+# Multiplicacion *
+# Division /
+# Concatenación .
+# Exponenciación **
+# Resto de una division %
+# Repeticion de caracteres x
+
+my $numero1 = 8;
+my $numero2 = 4;
+
+# Suma
+print $numero1 + $numero2."\n";
+
+# Resta
+print $numero1 - $numero2."\n";
+
+# Multiplicacion
+print $numero1 * $numero2."\n";
+
+# Division
+print $numero1 / $numero2."\n";
+
+# Concatenación
+print $numero1 . $numero2."\n";
+
+# Exponenciación
+print $numero1 ** $numero2."\n";
+
+# Resto de una division
+print $numero1 % $numero2."\n";
+
+# Repeticion de caracteres
+print $numero1 x $numero2."\n";
