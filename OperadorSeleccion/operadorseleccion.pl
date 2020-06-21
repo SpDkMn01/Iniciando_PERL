2c654437da42ed7a78d1cbc5174c56cfe5ad9a14
Pedro Diaz <pdiazstam@gmail.com>
2020-06-21 11:16:21 -0500

    Operador de Seleccion

---
+++ OperadorSeleccion/operadorseleccion.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $variable1 = 2;
+my $variable2 = 4;
+
+$variable1 < $variable2 ? print "OK\n" : print "Error\n";
+
+my $variable1 = 10;
+$variable1 < $variable2 ? print "OK\n" : print "Error\n";
