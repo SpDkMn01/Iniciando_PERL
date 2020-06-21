17d3159a613c2aec3c3d840c0d62d81709b48823
Pedro Diaz <pdiazstam@gmail.com>
2020-06-21 10:47:42 -0500

    Archivo de la primera clase

--- VariablesConstantes/variablesconstantes.pl
+++ VariablesConstantes/variablesconstantes.pl
@@ -7,3 +7,16 @@ use strict;
 my $variable = "Hola mundo";
 
 print $variable."\n";
+
+$variable = 30;
+print $variable."\n";
+
+# Definiendo una constante
+use constant pi => 3.14;
+
+print pi."\n";
+
+# El valor de la constante no puede variar
+# las siguientes lineas dan error
+# pi = "Hola";
+# print pi."\n";
